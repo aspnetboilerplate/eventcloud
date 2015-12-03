12dd701fe2e97b25c4ef553829f7ea3aabf478c7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-12-03 23:50:06 +0200

    Added BackchannelCertificateValidator for twitter login.

--- src/EventCloud.Web/App_Start/Startup.cs
+++ src/EventCloud.Web/App_Start/Startup.cs
@@ -65,7 +65,19 @@ namespace EventCloud.Web
             return new TwitterAuthenticationOptions
             {
                 ConsumerKey = ConfigurationManager.AppSettings["ExternalAuth.Twitter.ConsumerKey"],
-                ConsumerSecret = ConfigurationManager.AppSettings["ExternalAuth.Twitter.ConsumerSecret"]
+                ConsumerSecret = ConfigurationManager.AppSettings["ExternalAuth.Twitter.ConsumerSecret"],
+                BackchannelCertificateValidator =
+                    new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+                    {
+                        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                        "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                        "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+                        "B13EC36903F8BF4701D498261A0802EF63642BC3", // DigiCert High Assurance EV Root CA
+                        "add53f6680fe66e383cbac3e60922e3b4c412bed" //Symantec Class 3 EV SSL CA - G3
+                    })
             };
         }
 
