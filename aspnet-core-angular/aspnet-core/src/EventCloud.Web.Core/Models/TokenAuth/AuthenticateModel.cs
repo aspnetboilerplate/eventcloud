c070cf007e791e73ff76e89da6b970fac24dd9bc
felipealbuquerq <f.albuquerquedealmeida@gmail.com>
2018-03-17 06:24:58 -0300

    #2 - Atribuido o nome de exibição dos campos nome de usuário e senha

--- aspnet-core-angular/aspnet-core/src/EventCloud.Web.Core/Models/TokenAuth/AuthenticateModel.cs
+++ aspnet-core-angular/aspnet-core/src/EventCloud.Web.Core/Models/TokenAuth/AuthenticateModel.cs
@@ -6,10 +6,12 @@ namespace EventCloud.Models.TokenAuth
     public class AuthenticateModel
     {
         [Required]
+        [Display(Name = "Usuário ou E-mail", Description = "Nome de Usuário ou E-mail")]
         [StringLength(AbpUserBase.MaxEmailAddressLength)]
         public string UserNameOrEmailAddress { get; set; }
 
         [Required]
+        [Display(Name = "Senha", Description = "Senha do Usuário")]
         public string Password { get; set; }
         
         public bool RememberClient { get; set; }
