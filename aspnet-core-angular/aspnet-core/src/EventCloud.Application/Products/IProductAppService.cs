0be938671b3b431fdf5c77fd3cc5757d6ae03b04
thinker <felipe.almeida@thinkam.net>
2019-01-14 05:34:43 -0200

    #17 - identação do código

--- aspnet-core-angular/aspnet-core/src/EventCloud.Application/Products/IProductAppService.cs
+++ aspnet-core-angular/aspnet-core/src/EventCloud.Application/Products/IProductAppService.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace EventCloud.Products
 {
-using Abp.Application.Services;
-using Abp.Application.Services.Dto;
-using Abp.AutoMapper;
-using Dto;
+    using Abp.Application.Services;
+    using Abp.Application.Services.Dto;
+    using Abp.AutoMapper;
+    using Dto;
 
-public interface IProductAppService : IAsyncCrudAppService<ProductDto, Guid, PagedResultRequestDto, CreateProductDto, ProductDto>
-{
+    public interface IProductAppService : IAsyncCrudAppService<ProductDto, Guid, PagedResultRequestDto, CreateProductDto, ProductDto>
+    {
 
-}
+    }
 }
--- aspnet-core-angular/aspnet-core/src/EventCloud.Application/Products/ProductAppService.cs
+++ aspnet-core-angular/aspnet-core/src/EventCloud.Application/Products/ProductAppService.cs
@@ -2,19 +2,19 @@ using System;
 
 namespace EventCloud.Products
 {
-using Abp.Domain.Repositories;
-using Abp.Application.Services;
-using Abp.Application.Services.Dto;
-using Abp.Authorization;
-using Dto;
+    using Abp.Domain.Repositories;
+    using Abp.Application.Services;
+    using Abp.Application.Services.Dto;
+    using Abp.Authorization;
+    using Dto;
 
-public class ProductAppService : AsyncCrudAppService<Product, ProductDto, Guid, PagedResultRequestDto, CreateProductDto, ProductDto>, IProductAppService
-{
-private IRepository<Product, Guid> _productRepository;
+    public class ProductAppService : AsyncCrudAppService<Product, ProductDto, Guid, PagedResultRequestDto, CreateProductDto, ProductDto>, IProductAppService
+    {
+        private IRepository<Product, Guid> _productRepository;
 
-public ProductAppService(IRepository<Product, Guid> productRepository) : base(productRepository)
-{
-_productRepository = productRepository;
-}
-}
+        public ProductAppService(IRepository<Product, Guid> productRepository) : base(productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
 }
