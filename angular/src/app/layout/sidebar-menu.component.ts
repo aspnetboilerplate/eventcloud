6fbb5b5cebac92ee75e2acfa4b1c18adef04144f
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2024-01-23 13:21:20 +0300

    The localization deficiencies have been corrected.

--- angular/src/app/layout/sidebar-menu.component.ts
+++ angular/src/app/layout/sidebar-menu.component.ts
@@ -64,7 +64,7 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
             new MenuItem(
                 this.l('Events'),
                 '/app/events',
-                'fas fa-users',
+                'fas fa-calendar-days',
                 'Pages.Events'
             ),
             new MenuItem(this.l('MultiLevelMenu'), '', 'fas fa-circle', '', [
