75c59e5bcfc1e622e2cfef3b206df3cb9cafac7a
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2024-02-01 16:45:56 +0300

    fix wrongly setting tenantId problem

--- aspnet-core/src/EventCloud.Application/Events/EventAppService.cs
+++ aspnet-core/src/EventCloud.Application/Events/EventAppService.cs
@@ -61,7 +61,7 @@ namespace EventCloud.Events
 
         public async Task CreateAsync(CreateEventInput input)
         {
-            var tenantId = (int)AbpSession.UserId.Value;
+            var tenantId = (int)AbpSession.TenantId.Value;
             var @event = Event.Create(tenantId, input.Title, input.Date, input.Description, input.MaxRegistrationCount);
             await _eventManager.CreateAsync(@event);
         }
